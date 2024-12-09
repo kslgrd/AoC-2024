595d7ba3b7540a2b30dfe95294d800a99a6b2bcf
Keith Silgard <keith@thesilgards.com>
2024-12-08 17:05:36 -0800

    Try and fail to solve Day 6 Part 2

--- day6.ts
+++ day6.ts
@@ -16,6 +16,9 @@ type GridDirectionalLookup = {
   [key in Direction]: LookupTable;
 };
 
+const enableLog = true;
+const log = (msg: string) => enableLog && console.log(msg);
+
 // Initialize the grid and build some lookup tables to efficiently find obstacles
 const build2dGrid = (input: string) =>
   input.split("\n").map((row) => row.split(""));
@@ -30,53 +33,54 @@ const obstacle = "#";
 // Generate lookup tables that'll map every point on the grid to the position of the
 // nearest obstacle it'll hit when traveling in the provided direction
 // ... there has to be a better way to write this code, but _whatever_
-const lookupTables = directions.reduce<GridDirectionalLookup>((acc, d) => {
-  const lookupTable = Array.from({ length: rows }, () =>
-    Array(cols).fill(null)
-  ) as LookupTable;
+const generateLookupTables = (directions: Direction[], grid: Grid) =>
+  directions.reduce<GridDirectionalLookup>((acc, d) => {
+    const lookupTable = Array.from({ length: rows }, () =>
+      Array(cols).fill(null)
+    ) as LookupTable;
 
-  // iterate through each column and build the lookup table for traveling up/down
-  if (d === Direction.UP || d === Direction.DOWN) {
-    for (let col = 0; col < cols; col++) {
-      let obstaclePosition: Coord | null = null;
-      if (d === Direction.UP) {
-        // Start at the top, if we hit an obstacle note its position
-        // Anything beneath that will hit it if they're going up
-        for (let row = 0; row < rows; row++) {
-          if (grid[row][col] === obstacle) obstaclePosition = { row, col };
-          lookupTable[row][col] = obstaclePosition;
-        }
-      } else {
-        // Start at the bottom instead, but follow the same basic rules
-        for (let row = rows - 1; row >= 0; row--) {
-          if (grid[row][col] === obstacle) obstaclePosition = { row, col };
-          lookupTable[row][col] = obstaclePosition;
+    // iterate through each column and build the lookup table for traveling up/down
+    if (d === Direction.UP || d === Direction.DOWN) {
+      for (let col = 0; col < cols; col++) {
+        let obstaclePosition: Coord | null = null;
+        if (d === Direction.UP) {
+          // Start at the top, if we hit an obstacle note its position
+          // Anything beneath that will hit it if they're going up
+          for (let row = 0; row < rows; row++) {
+            if (grid[row][col] === obstacle) obstaclePosition = { row, col };
+            lookupTable[row][col] = obstaclePosition;
+          }
+        } else {
+          // Start at the bottom instead, but follow the same basic rules
+          for (let row = rows - 1; row >= 0; row--) {
+            if (grid[row][col] === obstacle) obstaclePosition = { row, col };
+            lookupTable[row][col] = obstaclePosition;
+          }
         }
       }
     }
-  }
 
-  // iterate through each row and build the lookup table for traveling left/right
-  if (d === Direction.LEFT || d === Direction.RIGHT) {
-    for (let row = 0; row < rows; row++) {
-      let obstaclePosition: Coord | null = null;
-      if (d === Direction.LEFT) {
-        for (let col = 0; col < cols; col++) {
-          if (grid[row][col] === obstacle) obstaclePosition = { row, col };
-          lookupTable[row][col] = obstaclePosition;
-        }
-      } else {
-        for (let col = cols - 1; col >= 0; col--) {
-          if (grid[row][col] === obstacle) obstaclePosition = { row, col };
-          lookupTable[row][col] = obstaclePosition;
+    // iterate through each row and build the lookup table for traveling left/right
+    if (d === Direction.LEFT || d === Direction.RIGHT) {
+      for (let row = 0; row < rows; row++) {
+        let obstaclePosition: Coord | null = null;
+        if (d === Direction.LEFT) {
+          for (let col = 0; col < cols; col++) {
+            if (grid[row][col] === obstacle) obstaclePosition = { row, col };
+            lookupTable[row][col] = obstaclePosition;
+          }
+        } else {
+          for (let col = cols - 1; col >= 0; col--) {
+            if (grid[row][col] === obstacle) obstaclePosition = { row, col };
+            lookupTable[row][col] = obstaclePosition;
+          }
         }
       }
     }
-  }
 
-  acc[d] = lookupTable;
-  return acc;
-}, {} as GridDirectionalLookup);
+    acc[d] = lookupTable;
+    return acc;
+  }, {} as GridDirectionalLookup);
 
 const findInitialPosition = (grid: Grid): Coord => {
   let initialPosition: Coord | undefined = undefined;
@@ -90,9 +94,6 @@ const findInitialPosition = (grid: Grid): Coord => {
   if (!initialPosition) throw new Error("Couldn't find initial position");
   return initialPosition;
 };
-
-const findObstacleInPath = (direction: Direction, { row, col }: Coord) =>
-  lookupTables[direction][row][col];
 
 const nextDirection = (direction: Direction) => {
   let nextIndex = directions.indexOf(direction) + 1;
@@ -134,9 +135,27 @@ const findExitPoint = (direction: Direction, { row, col }: Coord): Coord => {
   }
 };
 
+class HitError extends Error {}
+
 const solve = (grid: Grid) => {
   let direction = startingDirection;
   let guardPosition = findInitialPosition(grid);
+
+  const lookupTables = generateLookupTables(directions, grid);
+  const findObstacleInPath = (direction: Direction, { row, col }: Coord) =>
+    lookupTables[direction][row][col];
+
+  // Check if the guard every hits the same obstacle going in the same direction
+  // after hitting 3 other obstacles > if they do, they're in a loop
+  const hits: string[] = [];
+  const markHit = (direction: Direction, { row, col }: Coord) => {
+    const key = `${direction} [${row},${col}]`;
+    console.log(`- Hit at ${key}`);
+    hits.push(key);
+    for (let i = hits.length - 1; i >= 0; i -= 4) {
+      if (hits[i] === key) throw new HitError("Loop detected");
+    }
+  };
 
   // Use a map and a set so I don't have to care about duplicates
   const visited = new Map<number, Set<number>>();
@@ -149,31 +168,65 @@ const solve = (grid: Grid) => {
   };
 
   while (true) {
-    console.log(
-      `Moving ${direction} from row ${guardPosition.row}, col ${guardPosition.col}`
-    );
+    // log(
+    //   `Moving ${direction} from row ${guardPosition.row}, col ${guardPosition.col}`
+    // );
     const obstaclePosition = findObstacleInPath(direction, guardPosition);
     // The guard has left the room
     if (obstaclePosition === null) {
       // Pain in the ass... which cells does he visit on his way out?
       const exitPoint = findExitPoint(direction, guardPosition);
-      console.log(`- Exiting at row ${exitPoint.row}, col ${exitPoint.col}`);
+      // log(`- Exiting at row ${exitPoint.row}, col ${exitPoint.col}`);
       markVisited(positionsInPath(guardPosition, exitPoint));
-      console.log("👋 Bye guard!");
+      log("👋 Bye guard!");
       break;
     }
-    console.log(
-      `- Obstacle in path at row ${obstaclePosition.row}, col ${obstaclePosition.col}`
-    );
+    // log(
+    //   `- Obstacle in path at row ${obstaclePosition.row}, col ${obstaclePosition.col}`
+    // );
+    markHit(direction, obstaclePosition);
 
     direction = nextDirection(direction);
     const visitedPositions = markVisited(
       positionsInPath(guardPosition, obstaclePosition)
     );
-    guardPosition = visitedPositions[visitedPositions.length - 1];
+    // if we place an obstacle that forces an immediate turn visited positions will be empty
+    guardPosition =
+      visitedPositions[visitedPositions.length - 1] || guardPosition;
   }
 
   return [...visited.entries()].reduce((acc, [, cols]) => acc + cols.size, 0);
 };
 
-console.log(`Part 1: The guard visited ${solve(grid)} unique points`);
+// console.log(`Part 1: The guard visited ${solve(grid)} unique points`);
+
+const solvePart2 = (grid: Grid) => {
+  let loopingPositionCount = 0;
+  for (let row = 4; row < rows; row++) {
+    for (let col = 9; col < cols; col++) {
+      const cloneGrid = (grid: Grid): Grid => JSON.parse(JSON.stringify(grid));
+      const clonedGrid = cloneGrid(grid);
+      if (![obstacle, startingDirection].includes(clonedGrid[row][col])) {
+        log(`Setting obstacle at row ${row}, col ${col}`);
+        clonedGrid[row][col] = obstacle;
+        try {
+          solve(clonedGrid);
+        } catch (e) {
+          if (e instanceof HitError) {
+            loopingPositionCount++;
+            log(
+              `🔁 Loop ${loopingPositionCount} detected placing obstacle at row ${row}, col ${col}`
+            );
+          } else {
+            console.log("Unexpected error", e);
+          }
+        }
+      }
+    }
+  }
+  return loopingPositionCount;
+};
+
+console.log(
+  `Part 2: There are ${solvePart2(grid)} positions that create loops.`
+);
