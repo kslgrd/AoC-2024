1ec9c543bbe457d0c4d1d89a3defeca5de4d370d
Keith Silgard <keith@thesilgards.com>
2024-12-11 13:20:34 -0800

    Solve Day 11 Part 2

--- day11.ts
+++ day11.ts
@@ -5,8 +5,6 @@ const input = await file.text();
 
 const stones = input.split(" ");
 
-const blink = (stones: string[]) => stones.flatMap(onBlink);
-
 const onBlink = (stone: string): string[] => {
   // If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
   if (stone === "0") return ["1"];
@@ -25,11 +23,44 @@ const onBlink = (stone: string): string[] => {
   return [String(parseInt(stone, 10) * 2024)];
 };
 
-const repeatBlink = (stones: string[], count: number) =>
-  [...Array(count)].reduce((acc) => blink(acc), stones);
+const sum = (arr: number[]) => arr.reduce((acc, v) => acc + v);
+
+// We only care about the number of stones so we can probably speed things up a lot with memoization
+const blinkStoneCountMemo = new Map<string, number>();
+
+const getStoneCountAfterBlinks = (stone: string, blinks: number): number => {
+  if (blinks === 0) return 1;
+  const key = `${stone}|${blinks}`;
+  const cached = blinkStoneCountMemo.get(key);
+  if (cached) return cached;
+  // Recursively call the function until we hit zero
+  // Map it because `onBlink` will return two results for numbers with an even amount of digits
+  const counts = onBlink(stone).map((s) =>
+    getStoneCountAfterBlinks(s, blinks - 1)
+  );
+  const count = sum(counts);
+  // Cache so we never have to go through that hot mess ever again
+  blinkStoneCountMemo.set(key, count);
+  return count;
+};
+
+const solve = (stones: string[], blinks: number) => {
+  const stoneCounts = stones.map((s) => getStoneCountAfterBlinks(s, blinks));
+  return sum(stoneCounts);
+};
+
+const startTime = new Date().getMilliseconds();
+console.log(
+  `Part 1: we have ${solve(stones, 25)} stones after blinking 25 times.`
+);
 
 console.log(
-  `Part 1: we have ${
-    repeatBlink(stones, 25).length
-  } stones after blinking 25 times.`
+  `Part 2: we have ${solve(stones, 75)} stones after blinking 75 times.`
+);
+const endTime = new Date().getMilliseconds();
+
+console.log(
+  `Processed in ${endTime - startTime}ms. There are ${
+    blinkStoneCountMemo.size
+  } entries in the cache.`
 );
